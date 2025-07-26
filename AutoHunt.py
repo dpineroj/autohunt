b7df8d43d90e7862993dec029666bbcbc89072d8
David Pinero-Jacome <dpineroj@andrew.cmu.edu>
2025-07-25 20:21:33 -0400

    implemented countdown
    happens after grid is filled in by user. added delay. worked out kinks

--- AutoHunt.py
+++ AutoHunt.py
@@ -7,6 +7,7 @@
 
 from cmu_graphics import *
 
+
 def onAppStart(app):
     app.rows, app.cols = 4, 4
     app.cellSize = 100
@@ -16,6 +17,13 @@ def onAppStart(app):
     app.grid = [['' for _ in range(app.cols)] for _ in range(app.rows)]
     app.currentCell = 0
     app.gridSize = app.rows * app.cols
+    app.mode = 'input'
+
+    app.stepsPerSecond = 10
+    app.preCountdownSteps = 0
+    app.countdown = 3
+
+
 
 def drawGrid(app):
     for row in range(app.rows):
@@ -25,22 +33,38 @@ def drawGrid(app):
             #draw cell border
             drawRect(x, y, app.cellSize, app.cellSize, fill = None, 
                      border = 'black', borderWidth = 2)
-            #draw letter if cell is filled
-            letter = app.grid[row][col]
+            
+            letter = app.grid[row][col] #draw letter if cell is filled
             if letter:
                 drawLabel(letter, x + app.cellSize / 2, y + app.cellSize / 2,
                           size = 36, bold = True)
 
 def onKeyPress(app, key):
-    if key == 'backspace' and app.currentCell > 0:
-        app.currentCell -= 1
-        row, col = getGridPosition(app, app.currentCell)
-        app.grid[row][col] = ''
+    if app.mode == 'input':
+        if key == 'backspace' and app.currentCell > 0:
+            app.currentCell -= 1
+            row, col = getGridPosition(app, app.currentCell)
+            app.grid[row][col] = ''
 
-    elif len(key) == 1 and key.isalpha() and app.currentCell < app.gridSize:
-        row, col = getGridPosition(app, app.currentCell)
-        app.grid[row][col] = key.upper()
-        app.currentCell += 1
+        elif len(key) == 1 and key.isalpha() and app.currentCell < app.gridSize:
+            row, col = getGridPosition(app, app.currentCell)
+            app.grid[row][col] = key.upper()
+            app.currentCell += 1
+
+            if app.currentCell == app.gridSize:
+                app.mode = 'preCountdown'
+
+def onStep(app):
+    if app.mode == 'preCountdown':
+        app.preCountdownSteps += 1
+        if app.preCountdownSteps >= 5:
+            app.mode = 'countdown'
+            app.stepsPerSecond = 1
+    elif app.mode == 'countdown':
+        app.countdown -= 1
+        if app.countdown == 0:
+            app.mode = 'animate'
+
 
 #convert linear index to (row, col) coordinates
 def getGridPosition(app, index):
@@ -49,6 +73,10 @@ def getGridPosition(app, index):
 def redrawAll(app):
     drawLabel("Auto Hunt Beta", app.width // 2, 20, size = 20)
     drawGrid(app)
+
+    if app.mode == 'countdown':
+        drawLabel(app.countdown, app.width // 2, app.height // 1.05, 
+                  size = 24)
 
 runApp(width = 500, height = 500)
 
